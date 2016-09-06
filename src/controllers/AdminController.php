458ecafddac3477d85241877bb18348d32ac7cf0
markszabo <torin42@gmail.com>
2016-09-06 12:12:59 +0200

    Small bug fix

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -153,7 +153,7 @@ class AdminController extends Controller {
     $config = await Configuration::gen('game_duration_unit');
     $duration_unit = $config->getValue();
     $config = await Configuration::gen('game_duration_value');
-    $duration_value = intval($config->getValue());
+    $duration_value = $config->getValue();
 
     $minute_selected = $duration_unit === 'm';
     $hour_selected = $duration_unit === 'h';
