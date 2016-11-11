555d2bfe0c154be7012027297636c2276a839aea
ubuntu <justin@wraysec.com>
2016-11-11 01:58:43 -0500

    Reverted ScoreLog to upstream, implementing the PR to fix the memcached race condition (facebook/fbctf#352).

--- src/models/ScoreLog.php
+++ src/models/ScoreLog.php
@@ -69,7 +69,6 @@ class ScoreLog extends Model {
     $db = await self::genDb();
     await $db->queryf('DELETE FROM scores_log WHERE id > 0');
     self::invalidateMCRecords(); // Invalidate Memcached ScoreLog data.
-    Control::invalidateMCRecords('ALL_ACTIVITY'); // Invalidate Memcached Control data.
     MultiTeam::invalidateMCRecords('ALL_TEAMS'); // Invalidate Memcached MultiTeam data.
     MultiTeam::invalidateMCRecords('POINTS_BY_TYPE'); // Invalidate Memcached MultiTeam data.
     MultiTeam::invalidateMCRecords('LEADERBOARD'); // Invalidate Memcached MultiTeam data.
@@ -94,8 +93,8 @@ class ScoreLog extends Model {
           $level_capture_teams =
             $level_captures->get(intval($row->get('level_id')));
           invariant(
-            $level_capture_teams !== null,
-            'level_captures should not be null',
+            $level_capture_teams instanceof Vector,
+            'level_capture_teams should of type Vector and not null',
           );
           $level_capture_teams->add(intval($row->get('team_id')));
           $level_captures->set(
@@ -136,32 +135,30 @@ class ScoreLog extends Model {
         return false;
       }
     }
-    $level_captures = self::getMCRecords('LEVEL_CAPTURES');
-    invariant($level_captures !== null, 'level_captures should not be null');
     invariant(
-      $level_captures instanceof Map,
-      'level_captures should be of type Map',
+      $mc_result instanceof Map,
+      'cache return should of type Map and not null',
     );
-    if ($level_captures->contains($level_id)) {
+    if ($mc_result->contains($level_id)) {
       if ($any_team) {
-        $level_capture_teams = $level_captures->get($level_id);
+        $level_capture_teams = $mc_result->get($level_id);
         invariant(
-          $level_capture_teams !== null,
-          'level_capture_teams should not be null',
+          $level_capture_teams instanceof Vector,
+          'level_capture_teams should of type Vector and not null',
         );
         $team_id_key = $level_capture_teams->linearSearch($team_id);
-        if ($team_id_key != -1) {
+        if ($team_id_key !== -1) {
           $level_capture_teams->removeKey($team_id_key);
         }
         return intval(count($level_capture_teams)) > 0;
       } else {
-        $level_capture_teams = $level_captures->get($level_id);
+        $level_capture_teams = $mc_result->get($level_id);
         invariant(
-          $level_capture_teams !== null,
-          'level_capture_teams should not be null',
+          $level_capture_teams instanceof Vector,
+          'level_capture_teams should of type Vector and not null',
         );
         $team_id_key = $level_capture_teams->linearSearch($team_id);
-        return $team_id_key != -1;
+        return $team_id_key !== -1;
       }
     } else {
       return false;
@@ -220,7 +217,6 @@ class ScoreLog extends Model {
       $type,
     );
     self::invalidateMCRecords(); // Invalidate Memcached ScoreLog data.
-    Control::invalidateMCRecords('ALL_ACTIVITY'); // Invalidate Memcached Control data.
     MultiTeam::invalidateMCRecords('ALL_TEAMS'); // Invalidate Memcached MultiTeam data.
     MultiTeam::invalidateMCRecords('POINTS_BY_TYPE'); // Invalidate Memcached MultiTeam data.
     MultiTeam::invalidateMCRecords('LEADERBOARD'); // Invalidate Memcached MultiTeam data.
