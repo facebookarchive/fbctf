efac33eee88e0f276016599824db0be8afd19b9a
Javier Marcos <javierm@uber.com>
2016-11-21 11:24:01 -0800

    Make HHVM Repo Authoritative mode optional and change provision arguments

--- extra/provision.sh
+++ extra/provision.sh
@@ -20,7 +20,8 @@
 #   certbot Provision will generate a SSL certificate using letsencrypt/certbot. More info here: https://certbot.eff.org/
 #
 # Optional Parameters:
-#   -U,      --update      	 Pull from master GitHub branch and sync files to fbctf folder.
+#   -U,      --update            Pull from master GitHub branch and sync files to fbctf folder.
+#   -R,      --repo-mode         Enables HHVM Repo Authoritative mode to improve HHVM performance.
 #   -k PATH, --keyfile PATH      Path to supplied SSL key file.
 #   -C PATH, --certfile PATH     Path to supplied SSL certificate pem file.
 #   -D DOMAIN, --domain DOMAIN   Domain for the SSL certificate to be generated using letsencrypt.
@@ -75,6 +76,7 @@ function usage() {
   printf "  cerbot Provision will generate a SSL certificate using letsencrypt/certbot. More info here: https://certbot.eff.org/\n"
   printf "\nOptional Parameters:\n"
   printf "  -U,      --update \t\tPull from master GitHub branch and sync files to fbctf folder.\n"
+  printf "  -R,      --repo-mode \t\tEnables HHVM Repo Authoritative mode to improve HHVM performance.\n"
   printf "  -k PATH, --keyfile PATH \tPath to supplied SSL key file.\n"
   printf "  -C PATH, --certfile PATH \tPath to supplied SSL certificate pem file.\n"
   printf "  -D DOMAIN, --domain DOMAIN \tDomain for the SSL certificate to be generated using letsencrypt.\n"
@@ -90,7 +92,7 @@ function usage() {
   printf "\t%s -m dev -U -s /home/foobar/fbctf -d /var/fbctf\n" "${0}"
 }
 
-ARGS=$(getopt -n "$0" -o hm:c:Uk:C:D:e:s:d: -l "help,mode:,cert:,update,keyfile:,certfile:,domain:,email:,code:,destination:,docker" -- "$@")
+ARGS=$(getopt -n "$0" -o hm:c:URk:C:D:e:s:d: -l "help,mode:,cert:,update,repo-mode,keyfile:,certfile:,domain:,email:,code:,destination:,docker" -- "$@")
 
 eval set -- "$ARGS"
 
@@ -122,6 +124,10 @@ while true; do
       ;;
     -U|--update)
       UPDATE=true
+      shift
+      ;;
+    -R|--repo-mode)
+      REPOMODE=true
       shift
       ;;
     -k|--keyfile)
@@ -169,7 +175,7 @@ source "$CODE_PATH/extra/lib.sh"
 package git
 
 # Are we just updating a running fbctf?
-if [ "$UPDATE" == true ] ; then
+if [[ "$UPDATE" == true ]] ; then
     update_repo "$MODE" "$CODE_PATH" "$CTF_PATH"
     exit 0
 fi
@@ -272,8 +278,9 @@ import_empty_db "root" "$P_ROOT" "$DB" "$CTF_PATH" "$MODE"
 sudo chmod 777 "$CTF_PATH/src/data/attachments"
 sudo chmod 777 "$CTF_PATH/src/data/attachments/deleted"
 
-# If provisioning is in prod, improve HHVM performance
-if [[ "$MODE" == "prod" ]]; then
+# Check if HHVM Repo Authoritative mode needs to be enabled.
+# More info here: https://docs.hhvm.com/hhvm/advanced-usage/repo-authoritative
+if [[ "$REPOMODE" == true ]]; then
     hhvm_performance "$CTF_PATH"
 fi
 
