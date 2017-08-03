137112aeb9aeaa631562aea55664a11f0e9b786b
DaniLabs <danihzt@gmail.com>
2017-08-03 14:37:36 -0400

    Fix typo

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -2915,7 +2915,7 @@ class AdminController extends Controller {
             class={$highlighted_color}
             href="#"
             data-action={str_replace('_', '-', $highlighted_action)}>
-            {tr($current_status}}
+            {tr($current_status)}
           </a>;
       } else {
         $status_action = <a class={$highlighted_color}></a>;
